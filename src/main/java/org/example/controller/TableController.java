918ebc97c197438cce93b57832df5904533ca159
Robert <roberto_jokn@yahoo.com>
2023-01-04 00:12:03 +0200

    Table finalizat

--- src/main/java/org/example/controller/TableController.java
+++ src/main/java/org/example/controller/TableController.java
@@ -1,4 +1,50 @@
 package org.example.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.model.order.OrderRequest;
+import org.example.model.order.OrderResponse;
+import org.example.model.table.TableRequest;
+import org.example.model.table.TableResponse;
+import org.example.service.TableService;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("table")
+@RequiredArgsConstructor
+@Validated
 public class TableController {
+
+    private final TableService tableService;
+
+    @PostMapping("create")
+    public TableResponse createTable(@RequestBody @Valid TableRequest tableRequest) {
+        return tableService.createTable(tableRequest);
+    }
+
+    @GetMapping("list")
+    public List<TableResponse> getAllTables() {
+
+        return tableService.getAllTables();
+    }
+
+    @GetMapping("{id}")
+    public TableResponse findById(@PathVariable("id") Integer id) {
+
+        return tableService.findById(id);
+    }
+
+    @PatchMapping("update/{id}")
+    public void updateTableById(@PathVariable Integer id, @RequestBody @Valid TableRequest tableRequest) {
+        tableService.update(id, tableRequest);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteById(@PathVariable Integer id) {
+        tableService.deleteById(id);
+    }
+
 }
--- src/main/java/org/example/service/TableService.java
+++ src/main/java/org/example/service/TableService.java
@@ -1,18 +1,56 @@
 package org.example.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.entity.Order;
+import org.example.entity.Table;
+import org.example.exception.BusinessException;
+import org.example.mapper.TableMapper;
+import org.example.model.order.OrderRequest;
+import org.example.model.order.OrderResponse;
+import org.example.model.table.TableRequest;
+import org.example.model.table.TableResponse;
+import org.example.repository.TableRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class TableService {
 
+    private final TableRepository tableRepository;
 
+    private final TableMapper tableMapper;
 
+    public TableResponse createTable(TableRequest tableRequest) {
+        Table table = tableMapper.map(tableRequest);
+        return tableMapper.map(tableRepository.save(table));
+    }
 
+    public List<TableResponse> getAllTables() {
+        return tableMapper.map(tableRepository.findAll());
+    }
+
+    public TableResponse findById(Integer id) {
+        Table table = tableRepository.findById(id).orElseThrow(() -> new BusinessException("Cannot find the table with ID" + id));
+        return tableMapper.map(table);
+    }
+
+    public void update(Integer id, TableRequest tableRequest) {
+        Table tableToUpdate = tableRepository.findById(tableRequest.getId()).orElseThrow(
+                () -> new BusinessException(String.format("Table with id: %s is not found", id))
+        );
+        tableToUpdate.setId(tableRequest.getId());
+        tableToUpdate.setNumber(tableRequest.getNumber());
+        tableToUpdate.setSeat(tableRequest.getSeat());
+    }
+
+    public void deleteById(Integer id) {
+        Table table = tableRepository.findById(id).orElseThrow(() -> new BusinessException("Cannot find the table with ID" + id));
+        tableRepository.delete(table);
+    }
 
 
 }
